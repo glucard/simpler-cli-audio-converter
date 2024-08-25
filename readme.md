1b981955f7149af0b0a034e1f13a9bc840c64383
george <georgezambonin@gmail.com>
2024-08-24 23:53:01 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# SIMPLER CLI AUDIO CONVERTER
+
+Simplifier audio convertion on folders struct
+
+## Requirements
+
+- ffmpeg
+- python
+
+## Usage
+
+### Linux/WSL
+
+```bash
+python3 scac "path/to/source_audios_folder" <target_format> "path/to/converted_audios_dest" 
+```
---
+++ scac.py
@@ -0,0 +1,45 @@
+# scac (simpler cli audio converter)
+
+import argparse
+import os
+import librosa
+from pydub import AudioSegment
+
+def convert_audio(input_path:str, output_path:str, source_format:str, target_format:str):
+    audio = AudioSegment.from_file(input_path, codec=source_format)
+    audio.export(output_path, format=target_format)
+
+def convert_dir(input_path: str, output_path: str, source_format:str, target_format:str):
+    if os.path.isdir(input_path):
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+
+        for p in os.listdir(input_path):
+            convert_dir(os.path.join(input_path, p), os.path.join(output_path, p), source_format, target_format)
+
+    if output_path.split('.')[-1] == source_format:
+        convert_audio(input_path, output_path, source_format, target_format)
+        print(f"{input_path} to {output_path}.{target_format}")
+
+    return
+
+def main():
+    parser = argparse.ArgumentParser(description="A simple audio converter CLI tool.")
+    parser.add_argument("source_path", help="Path to the source audios dir.")
+    parser.add_argument("target_path", help="Path to the output audios dir.")
+    parser.add_argument("--sf", default="opus", help="Source audio format (default: opus)")
+    parser.add_argument("--tf", default="wav", help="Output audio format (default: wav)")
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.source_path):
+        raise ValueError(f"{args.source_path} is not a dir.")
+    
+    if not os.path.isdir(args.target_path):
+        os.mkdir(args.target_path)
+        # raise ValueError(f"{args.target_path} is not a dir.")
+    
+    convert_dir(args.source_path, args.target_path, args.sf, args.tf)
+
+if __name__ == "__main__":
+    main()
